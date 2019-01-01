d82ed923bfd49b9b2836be66c6e144318c26bd19
iuv <zhuoxun.yang@gometech.com.cn>
2019-01-01 15:28:56 +0800

    add other files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './src/App';
+import FlexTest from './src/01_flex_demo/FlexTest';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FlexTest);
